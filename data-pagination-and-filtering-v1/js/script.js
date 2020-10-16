ddb1ee2dc165bbca0c1c53a11f685f129d54d415
Eddie <eddiedelfert@gmail.com>
2020-10-16 16:34:37 -0700

    created addPagination function
    first draft of project. cleaned code and added comments. bug when clicking between or around buttons.

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -28,6 +28,7 @@ function showPage(list, page) {
    // loop over the length of the `list` parameter
    for (let i = 0; i < list.length; i++){
       if (firstStudent <= i && i < lastStudent) {
+        //create elements to display student information
          const li = document.createElement("LI")
          const div = document.createElement("DIV")
          const img = document.createElement("IMG")
@@ -35,19 +36,19 @@ function showPage(list, page) {
          const span = document.createElement ("SPAN")
          const div2 = document.createElement ("DIV")
          const span2 = document.createElement ("SPAN")
-
+        //make elements have respective class names.
          li.className = "student-item cf"
          div.className = "student-details"
          img.className = "avatar"
          span.className = "email"
          div2.className = "joined-details"
          span2.className = "date"
-
+        // add data to elements using loop
          h3.textContent = `${data[i].name.first} ${data[i].name.last}`
          span.textContent = `${data[i].email}`
          img.src = `${data[i].picture.large}`
          span2.textContent = `${data[i].registered.date}`
-
+        // add elements to the page
          studentList.appendChild(li)
          li.appendChild(div)
          div.appendChild(img)
@@ -63,48 +64,45 @@ function showPage(list, page) {
        
    }
 
-   showPage(data, 1)
-     // inside the loop create a conditional to display the proper students
-       // inside the conditional:
-         // create the elements needed to display the student information
-         // insert the above elements
- 
-
-
 /*
 Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+Add page numbers and change data based on number clicked. 
 */
 
+function addPagination (list) {
+  //create variable to store page numbers needed & select link-list class
+  const buttons = Math.ceil(list.length / 9)
+  const elementUl = document.querySelector(".link-list")
+  //set Ul to blank string
+  elementUl.innerHTML = ''
+  // loop over variable for number of pages needed 
+  for (let i = 1; i < buttons; i++) {
+    //create elements for buttons 
+    const li = document.createElement('LI')
+    const button = document.createElement('BUTTON')
+    //change button string dynamically 
+    button.type = "button"
+    button.innerHTML = i
+     if (button.innerHTML === "1") {
+      button.className = "active";
+    } 
+    // add elements to page 
+    elementUl.appendChild(li)
+    li.appendChild(button)
 
+    //click event to change data on number clicked
+    elementUl.addEventListener('click', (e) => { 
+      button.className = ""
+      const target = e.target;
+
+      target.className = "active"
+
+      showPage(data, target.innerHTML)
+
+       })
+  }
+}
 
 // Call functions
-
-
-/*
-Code that I'm not sure will work as i want it to 
-*/
-
-// const li = document.createElement("LI")
-// const div = document.createElement("DIV")
-// const img = document.createElement("IMG")
-// const h3 = document.createElement("H3")
-// const span = document.createElement ("SPAN")
-// const div2 = document.createElement ("DIV")
-// const span2 = document.createElement ("SPAN")
-
-// li.className = "student-item cf"
-// div.className = "student-details"
-// img.className = "avatar"
-// span.className = "email"
-// div2.className = "joined-details"
-// span.className = "date"
-
-// document.body.appendChild(li)
-// li.appendChild(div)
-// div.appendChild(img)
-// div.appendChild(h3)
-// div.appendChild(span)
-// li.appendChild(div2)
-// div2.appendChild(span2)
-// }
+showPage(data, 1)
+addPagination(data)
